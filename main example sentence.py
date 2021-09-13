ff2cca94f139b20f2e73c0005fb444bf8b215b0f
Calvin Xu <calvinxu806@gmail.com>
2021-09-12 23:37:02 -0700

    Update (for new sentence-based questions)

--- main example sentence.py
+++ main example sentence.py
@@ -3,8 +3,8 @@ import requests
 import time
 import shutil
 
-for i in range(6, 7 + 1):
-	url = "https://jlptsensei.com/jlpt-n1-grammar-list/page/" + str(i) + "/"
+for i in range(1, 5 + 1):
+	url = "https://jlptsensei.com/jlpt-n2-grammar-list/page/" + str(i) + "/"
 	page = requests.get(url)
 	soup = BeautifulSoup(page.text, "lxml")
 	
@@ -22,24 +22,30 @@ for i in range(6, 7 + 1):
 			english = englishColumn.text
 			
 			# for continue in the middle
-			if int(index) > 200:
-				f = open("n3.txt", "a")
-				f.write(index + "|" + japanese + "|" + english + "\n")
-				f.close()
+			if int(index) > 0:
+#				f = open("n3.txt", "a")
+#				f.write(index + "|" + japanese + "|" + english + "\n")
+#				f.close()
 				
 				# get image
 				try:
 					japaneseHref = japaneseColumn.a["href"]
 					flashCardPage = requests.get(japaneseHref)
 					flashCardSoup = BeautifulSoup(flashCardPage.text, "lxml")
-					imageDiv = flashCardSoup.find_all("div", {"class": "text-center mt-4 grammar-thumbnail-cont"})[0]
-					imageUrl = next(imageDiv.children, None).a["href"]
-					img_data = requests.get(imageUrl).content
-					with open(str(index) + ".png", "wb") as handler:
-						handler.write(img_data)
+					exampleDiv = flashCardSoup.find_all("p", {"class": "m-0 jp"})[0]
+					exampleSentence = exampleDiv
+					print(exampleSentence)
+					f = open("n2.txt", "a")
+					f.write(index + "|" + str(exampleSentence) + "\n")
+					f.close()
+#					imageDiv = flashCardSoup.find_all("div", {"class": "text-center mt-4 grammar-thumbnail-cont"})[0]
+#					imageUrl = next(imageDiv.children, None).a["href"]
+#					img_data = requests.get(imageUrl).content
+#					with open(str(index) + ".png", "wb") as handler:
+#						handler.write(img_data)
 				except AttributeError:
 					print("Unable to find image for", index)
-				time.sleep(1)
+#				time.sleep(1)
 		except IndexError:
 			print("Unable to parse row")
 		
